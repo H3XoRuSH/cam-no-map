9f46ab5954cb319d78fe0b9c52bd80db0bcb2590
Gabriel Samonte <samontegabriel2001@gmail.com>
2023-01-18 12:12:04 +0800

    Prototype Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import Map, {Marker} from 'react-map-gl';
+import React, { useState } from 'react';
+import Sidenav from './Components/Sidenav';
+import { locations } from "./lib/navData";
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiaDN4b3J1c2giLCJhIjoiY2w4Mmxka2kzMHF3eDNvcnUzeTlyaGIxbCJ9.xtXQEOU-6q-ZXGXO3bLjeA';
+// pk.eyJ1IjoiaDN4b3J1c2giLCJhIjoiY2w4MmxqZDRlMDZ3NTN2bXhtZWwyeHlzbCJ9.hq4G-I8Nl8qOJc6jOC8gHA
 
 function App() {
+  const [myState, setMyState] = useState({
+    center_long: 122.94582373167115,
+    center_lat: 14.12175226941637,
+    mark: [0, 0, 0, 0]
+  })
+  
+  const toggle = (idx) => {
+    setMyState(curState => {
+      const newMark = [...curState.mark]
+      newMark[idx] = !newMark[idx]
+      return {
+        ...curState,
+        mark: newMark
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div position="relative">
+      <Map
+      initialViewState={{
+        latitude: 14.12175226941637,
+        longitude: 122.94582373167115,
+        zoom: 14
+      }} 
+      style={{position: 'absolute'}}
+      mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      {
+       myState.mark[0] && 
+       locations[0].map(item => {
+        return <Marker longitude={item.long} latitude={item.lat} color="#C23B23"/>
+       })
+      }
+      {
+       myState.mark[1] && 
+       locations[1].map(item => {
+        return <Marker longitude={item.long} latitude={item.lat} color="#03C03C"/>
+       })
+      }
+      {
+       myState.mark[2] && 
+       locations[2].map(item => {
+        return <Marker longitude={item.long} latitude={item.lat} color="#579ABE"/>
+       })
+      }
+      {
+       myState.mark[3] && 
+       locations[3].map(item => {
+        return <Marker longitude={item.long} latitude={item.lat} color="#976ED7"/>
+       })
+      }
+      
+    </Map>
+    <Sidenav toggle={toggle} />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Sidenav.js
@@ -0,0 +1,65 @@
+import styles from "./sidenav.module.css"
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import { useState } from "react";
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+export default function Sidenav(props) {
+    const [open, setopen] = useState(false)
+    const [marker0, setmarker0] = useState(false)
+    const [marker1, setmarker1] = useState(false)
+    const [marker2, setmarker2] = useState(false)
+    const [marker3, setmarker3] = useState(false)
+    const [marker4, setmarker4] = useState(false)
+    const toggleOpen = () => {
+        setopen(!open)
+    }
+    const toggleMarker0 = () => {
+        setmarker0(!marker0)
+        props.toggle(0)
+    }
+    const toggleMarker1 = () => {
+        setmarker1(!marker1)
+        props.toggle(1)
+    }
+    const toggleMarker2 = () => {
+        setmarker2(!marker2)
+        props.toggle(2)
+    }
+    const toggleMarker3 = () => {
+        setmarker3(!marker3)
+        props.toggle(3)
+    }
+  return (
+    <div className={open?styles.sidenav:styles.sidenavClosed}>
+        <button className={styles.menuBtn} onClick={toggleOpen}>
+            {open? <KeyboardDoubleArrowLeftIcon />: <KeyboardDoubleArrowRightIcon />}
+        </button>
+        <div className={styles.sideitem}>
+            <button className={styles.marker1} onClick={toggleMarker0}>
+                {marker0? <CheckBoxIcon/> :<CheckBoxOutlineBlankIcon/>}
+            </button>
+            <span className={styles.linkText}>Accommodation</span>
+        </div>
+        <div className={styles.sideitem}>
+            <button className={styles.marker2} onClick={toggleMarker1}>
+                {marker1? <CheckBoxIcon/> :<CheckBoxOutlineBlankIcon/>}
+            </button>
+            <span className={styles.linkText}>Tourism</span>
+        </div>
+        <div className={styles.sideitem}>
+            <button className={styles.marker3} onClick={toggleMarker2}>
+                {marker2? <CheckBoxIcon/> :<CheckBoxOutlineBlankIcon/>}
+            </button>
+            <span className={styles.linkText}>Historical</span>
+        </div>
+        <div className={styles.sideitem}>
+            <button className={styles.marker4} onClick={toggleMarker3}>
+                {marker3? <CheckBoxIcon/> :<CheckBoxOutlineBlankIcon/>}
+            </button>
+            <span className={styles.linkText}>Business</span>
+        </div>
+    </div>
+  )
+}
